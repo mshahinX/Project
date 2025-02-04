fd4d8b65c21426e7d36fb73203745832a67dc475
Shahin Mammadzade <155653536+tlsshahin@users.noreply.github.com>
2025-02-04 06:14:37 +0400

    Your commit message describing the changes

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from application import app, db
 from flask import render_template
 from flask import request,json,Response
+from application.models import User,Course,Enrollment
 
 courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
 
@@ -41,12 +42,6 @@ def api(idx=None):
     
     return Response(json.dumps(jdata),mimetype= "application/json")
 
-class User(db.Document):
-    user_id = db.IntField(unique=True)
-    first_name = db.StringField(max_length = 50)
-    last_name = db.StringField(max_length =50)
-    email = db.StringField(max_length = 30)
-    password = db.StringField(max_length = 30)
 
 
 @app.route("/user")
