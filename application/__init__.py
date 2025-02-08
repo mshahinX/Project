808de8181fd434b6fa729585323fc2eb2653aa33
mshahinX <memmedovasim782@gmail.com>
2025-02-08 10:48:28 +0400

    Fix

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
-
+import os
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -11,4 +11,6 @@ db.init_app(app)
 
 from application import routes
 
-
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
